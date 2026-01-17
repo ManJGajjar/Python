82a3529c6af04307da191adbd7843ce49a3a1230
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-01-17 23:12:26 +0530

    Update Datatype.py

--- Datatype.py
+++ Datatype.py
@@ -1,7 +1,7 @@
-name = "Man"
-age  = 18
+name = "Peter"
+age  = 19
 weight = 80.01
-Student = True
+Student = False
 xyz = None
 
 print(type(name)) #This will return the type pf name variable
@@ -9,4 +9,5 @@ print(type(age)) #This will return the type of age variable
 print(type(weight)) #This will return the type of weight variable
 print(type(Student)) #This will return the type of Student variable
 print(type(xyz)) #This will return the type of xyz variable
+
 print("This are mainly 5 datatypes in python.")
