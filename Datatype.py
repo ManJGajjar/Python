600ad04c1c593d457c55fcbe7d940257b085774c
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-01-17 23:11:29 +0530

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,12 @@
+name = "Man"
+age  = 18
+weight = 80.01
+Student = True
+xyz = None
+
+print(type(name)) #This will return the type pf name variable
+print(type(age)) #This will return the type of age variable
+print(type(weight)) #This will return the type of weight variable
+print(type(Student)) #This will return the type of Student variable
+print(type(xyz)) #This will return the type of xyz variable
+print("This are mainly 5 datatypes in python.")
---
+++ TypeCasting.py
@@ -0,0 +1,11 @@
+x = "25"
+y = 10.5
+name = 254
+
+result1 = int(x)
+result2 = int(y)
+result3 = str(name)
+
+print(type(result1))
+print(type(result2))
+print(type(result3))
