90647a40284d72474f2611645dd43d4884a1289b
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-02-19 22:19:22 +0530

    Add files via upload

---
+++ Attributes.py
@@ -0,0 +1,9 @@
+class Employee:
+   name = "Bhavesh Aggarwal"
+   age = "30"
+
+# instance of the class
+emp = Employee()
+# accessing class attributes
+print("Name of the Employee:", emp.name)
+print("Age of the Employee:", emp.age)
