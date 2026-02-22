06c9f3a974d13072a6a9b9c4aa511152c2cc105f
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-02-22 23:36:54 +0530

    Add files via upload

---
+++ ArithmeticOperator.py
@@ -0,0 +1,16 @@
+a = 15
+b = 4
+
+print("Addition:", a + b)  
+
+print("Subtraction:", a - b) 
+
+print("Multiplication:", a * b)  
+
+print("Division:", a / b) 
+
+print("Floor Division:", a // b)  
+
+print("Modulus:", a % b) 
+
+print("Exponentiation:", a ** b)
