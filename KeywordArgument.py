c6a6ea6d404334d2ecaa2376386dc2bc3953431d
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-02-25 22:42:32 +0530

    Add files via upload

---
+++ KeywordArgument.py
@@ -0,0 +1,10 @@
+# Function definition is here
+def printinfo( name, age ):
+   "This prints a passed info into this function"
+   print ("Name: ", name)
+   print ("Age ", age)
+   return
+
+printinfo ("Naveen", 29)
+
+printinfo(name="miki", age = 30)
