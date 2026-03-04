2f57ed8e54914f1ba9eddc22854890a359a9b5f2
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-03-04 23:28:56 +0530

    Add files via upload

---
+++ Interface.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class demoInterface(ABC):
+   @abstractmethod
+   def method1(self):
+      print ("Abstract method1")
+      return
+
+   @abstractmethod
+   def method2(self):
+      print ("Abstract method1")
+      return
