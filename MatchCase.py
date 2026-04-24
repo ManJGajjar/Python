aff176f70b2e1513c44b673e50775bd50e6749dc
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-04-24 23:09:18 +0530

    Add files via upload

---
+++ MatchCase.py
@@ -0,0 +1,13 @@
+def weekday(n):
+   match n:
+      case 0: return "Monday"
+      case 1: return "Tuesday"
+      case 2: return "Wednesday"
+      case 3: return "Thursday"
+      case 4: return "Friday"
+      case 5: return "Saturday"
+      case 6: return "Sunday"
+      case _: return "Invalid day number"
+print (weekday(3))
+print (weekday(6))
+print (weekday(7))
