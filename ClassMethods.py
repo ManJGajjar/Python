7793a65f7da58e2e04fc0ca00975faa92fa1710e
Man J Gajjar <131265476+ManJGajjar@users.noreply.github.com>
2026-04-29 23:43:43 +0530

    Add files via upload

---
+++ ClassMethods.py
@@ -0,0 +1,17 @@
+class Employee:
+   empCount = 0
+   def __init__(self, name, age):
+      self.__name = name
+      self.__age = age
+      Employee.empCount += 1
+   def showcount(self):
+      print (self.empCount)
+      
+   counter = classmethod(showcount)
+
+e1 = Employee("Bhavana", 24)
+e2 = Employee("Rajesh", 26)
+e3 = Employee("John", 27)
+
+e1.showcount()
+Employee.counter()
